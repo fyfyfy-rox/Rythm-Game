c88aca66fa6b8099c97cbb01992692c7defc8818
Luis Melzer <luis.melzer.m@gmail.com>
2024-11-19 18:26:51 +0100

    jetzt aber wirklich alle Datien

---
+++ Scenen/area_2d.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body: PhysicsBody2D):
+	entered = true
+func _on_body_exited(body: Node2D):
+	entered = false
+	
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene_to_file("res://Scenen/scene_2.tscn")
